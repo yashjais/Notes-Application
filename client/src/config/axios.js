f5c8803a64bd77fa7fa3222a5c11363205c9c807
Yash Jaiswal <jais.yash01@gmail.com>
2020-03-15 22:38:58 +0530

    index change

--- client/src/config/axios.js
+++ client/src/config/axios.js
@@ -2,7 +2,7 @@ import Axios from 'axios'
 
 const axios = Axios.create({
     // baseURL: 'http://localhost:3020' // for dev
-    baseURL: 'https://yash-notes.herokuapp.com' // for production
+    baseURL: '' // for production
 })
 
 export default axios
